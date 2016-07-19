4cfb91795ab7f5b00d33e273e83bfa6dc639aa4b
ajn123 <ajn123@vt.edu>
2016-07-19 10:55:49 -0500

    fixes to be made

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,17 @@ module ApplicationHelper
 	  markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
 	  markdown_to_html.render(text).html_safe
 	end
+
+    def resource_name
+       :user
+    end
+
+   def resource
+         @resource ||= User.new
+   end
+
+    def devise_mapping
+          @devise_mapping ||= Devise.mappings[:user]
+    end
+
 end
